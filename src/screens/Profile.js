bbd8ec2bde415f48bf174268691abe433b122e1f
emmanuelojiji <emmanuelojiji@hotmail.com>
2025-07-22 00:57:16 +0100

    feat: profile page pulls user from firestore

--- src/screens/Profile.js
+++ src/screens/Profile.js
@@ -1,11 +1,43 @@
+import {
+  collection,
+  doc,
+  getDoc,
+  getDocs,
+  query,
+  where,
+} from "firebase/firestore";
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import { db } from "../firebaseConfig";
 
 const Profile = () => {
   const params = useParams();
 
+  const [username, setUsername] = useState("");
+
+  const getUser = async () => {
+    try {
+      const usersRef = collection(db, "users");
+      const q = query(usersRef, where("username", "==", `${params.username}`));
+      const userDoc = await getDocs(q);
+
+      userDoc.forEach((doc) => {
+        setUsername(doc.data().username);
+      });
+
+      console.log(userDoc);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    getUser();
+  }, []);
+
   return (
     <div className="Profile">
-      <h1>{params.username}</h1>
+      <h1>{username}</h1>
     </div>
   );
 };
