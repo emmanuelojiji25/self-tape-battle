6aea79fa1a5ac77113bee58fa934550a2a7184c4
emmanuelojiji <emmanuelojiji@hotmail.com>
2025-12-30 14:34:30 +0000

    feat: outlet added to sidemenu

--- src/components/SideMenu.js
+++ src/components/SideMenu.js
@@ -1,21 +1,47 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./SideMenu.scss";
 
 const SideMenu = ({ slideIn, toggleMenu }) => {
   const [view, setView] = useState("none");
 
+  const [outletSlideIn, setOutletSlideIn] = useState(false);
+
+  const handleOutletToggle = (nextView) => {
+    if (view === "none") {
+      setView(nextView);
+
+      setTimeout(() => {
+        setOutletSlideIn(true);
+      }, 10);
+    } else {
+      setOutletSlideIn(false);
+
+      setTimeout(() => {
+        setView("none");
+      }, 300);
+    }
+  };
+
+  useEffect(() => {
+    console.log(view);
+  }, [view]);
+
   return (
     <div className={`SideMenu ${slideIn && "slideIn"}`}>
       <h1 onClick={toggleMenu}>close</h1>
       <p>Profile</p>
-      <p>How to play</p>
-      <p>Submit feedback</p>
-      <p>Contact</p>
+      <p onClick={() => handleOutletToggle("how-to-play")}>How to play</p>
+      <p onClick={() => handleOutletToggle("submit-feedback")}>
+        Submit feedback
+      </p>
+      <p onClick={() => handleOutletToggle("contact")}>Contact</p>
       <p>Sign out</p>
 
-      <div className="outlet">
-        <h2>title</h2>
-      </div>
+      {view !== "none" && (
+        <div className={`outlet ${outletSlideIn && "outletSlideIn"}`}>
+          <h2 onClick={() => handleOutletToggle()}>back</h2>
+        </div>
+      )}
     </div>
   );
 };
