da202883c4a7abf6d4214023d4dd8ce27d1724a4
emmanuelojiji <emmanuelojiji@hotmail.com>
2026-02-02 04:36:29 +0000

    fix: battles entered amount fix

--- src/screens/Battle.js
+++ src/screens/Battle.js
@@ -237,7 +237,7 @@ const Battle = () => {
           await updateDoc(userRef, {
             coins: increment(1),
             totalCoinsEarned: increment(5),
-            battlesEntered: increment(5),
+            battlesEntered: increment(1),
           });
 
           await updateDoc(userRef, {
