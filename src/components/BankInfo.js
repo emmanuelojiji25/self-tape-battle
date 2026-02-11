03fffd2a16c81149dcccc2f8e7ec08a38def7f9c
emmanuelojiji <emmanuelojiji@hotmail.com>
2026-02-11 22:08:32 +0000

    fix: button component added to BankInfo

--- src/components/BankInfo.js
+++ src/components/BankInfo.js
@@ -1,31 +1,31 @@
+import Button from "./Button";
 import Input from "./Input";
 
-
-
-const BankInfo = ({user}) => {
-    return(
-        <div className="edit-profile-section">
-          <h2>Bank details</h2>
-          <Input
-            type="text"
-            placeholder="Account name"
-            value={user.accountName}
-            onChange={(e) => setAccountName(e.target.value)}
-          />
-          <Input
-            type="text"
-            placeholder="Account number"
-            value={user.accountNumber}
-            onChange={(e) => setAccountNumber(e.target.value)}
-          />
-          <Input
-            type="text"
-            placeholder="Sort code"
-            value={user.sortCode}
-            onChange={(e) => setSortCode(e.target.value)}
-          />
-        </div>
-    )
-}
+const BankInfo = ({ user }) => {
+  return (
+    <div className="edit-profile-section">
+      <h2>Bank details</h2>
+      <Input
+        type="text"
+        placeholder="Account name"
+        value={user.accountName}
+        onChange={(e) => setAccountName(e.target.value)}
+      />
+      <Input
+        type="text"
+        placeholder="Account number"
+        value={user.accountNumber}
+        onChange={(e) => setAccountNumber(e.target.value)}
+      />
+      <Input
+        type="text"
+        placeholder="Sort code"
+        value={user.sortCode}
+        onChange={(e) => setSortCode(e.target.value)}
+      />
+      <Button text="Save" filled_color />
+    </div>
+  );
+};
 
 export default BankInfo;
